13f55e66b720417e127d059cbb5bae131dfc1153
dyabk <dzmitry.yabkevich@outlook.com>
2022-01-17 13:18:35 -0500

    Used the method of chained method calls to access the response data of the first promise

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import App from './App';
 
 import axios from 'axios';
 
-const promise = axios.get('http://localhost:3001/notes')
-console.log(promise)
-
-promise.then(response => {
-  console.log(response)
+axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data  
+  console.log(notes)
 })
 
 const promise2 = axios.get('http://localhost:3001/foobar')
